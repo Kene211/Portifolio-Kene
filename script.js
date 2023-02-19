dcac914282a386cf709bcdf491bddab84d6e8493
Kene Pale <kenepale@gmail.com>
2023-02-20 00:29:06 +0200

    Main Home
    Site

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", 
+{
+    String: [".Navegador Aereo", ".Operador de RPAS", "Programador Web e Mobile"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+}
+)
